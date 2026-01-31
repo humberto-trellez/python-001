753254c0babe3b1fb78c639424c606f545c870ba
htrellez <htrellez@icloud.com>
2026-01-31 06:05:06 -0600

    estructuras de control

---
+++ estructuras_de_control.py
@@ -0,0 +1,10 @@
+calificacion = 8.5
+
+if calificacion >= 9.0:
+    print ("Excelente.")
+elif calificacion >= 8.0:
+    print ("Muy bueno")
+elif calificacion >= 7.0:
+    print ("Bueno")
+else:
+    print ("Necesitas mejorar")
