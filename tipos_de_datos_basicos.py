7aa72132f5a20225bf6819703226641a011efc79
htrellez <htrellez@icloud.com>
2026-01-31 05:43:06 -0600

    tipos de datos basicos

---
+++ tipos_de_datos_basicos.py
@@ -0,0 +1,19 @@
+# int
+edad = 40
+cantidad = 300
+
+# float
+precio = 8.49
+altura = 1.82
+
+# strings
+nombre = "Pedro"
+mensaje = 'Bienvenido a Python'
+mensaje_largo = """ Este es 
+un mensaje de varias
+lineas
+"""
+
+# boolean
+es_alto = True
+es_valido = False
