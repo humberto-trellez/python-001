be9590b5d141e82fb7d6ad086a70abe1f8c0cba7
htrellez <htrellez@icloud.com>
2026-01-31 05:56:53 -0600

    operadores

---
+++ operadores.py
@@ -0,0 +1,24 @@
+# aritmeticos
+x = 20
+y = 2
+
+suma = x + y
+resta = x - y
+multiplicacion = x - y
+division = x / y
+division_entera = x // y
+modulo = x % y
+exponenciacion = x ** y
+
+# de comparacion
+igual = x == y
+diferente = x != y
+mayor_que = x > y
+menor_que = x < y
+mayor_o_igual = x >= y
+menor_o_igual = x <= y
+
+# logicos
+resultado_and = (x > 10) and (y < 10)
+resultado_or = (x > 10) or (y < 10)
+resultado_not = not (x > 10)
