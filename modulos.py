b6e453a765c5157b658d6069eb5c2a37cb2c4f7d
htrellez <htrellez@icloud.com>
2026-01-31 20:13:54 -0600

    modulos

---
+++ modulos.py
@@ -0,0 +1,12 @@
+from random import randint
+import datetime
+
+numero_aleatorio = randint(1, 10)
+print(numero_aleatorio)
+
+fecha_actual = datetime.datetime.now()
+print(fecha_actual)
+
+print((not True) or (False and True))
+
+print(5 + int("3"))
